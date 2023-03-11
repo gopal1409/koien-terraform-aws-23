63869b4fe1095c7dc63ecb8c2248527b19baf180
gopal das <gopal1409@gmail.com>
2023-03-11 21:13:49 +0530

    terraform initializing

---
+++ provider.tf
@@ -0,0 +1,19 @@
+###before i do terraform init i need to provide the provider block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+}
